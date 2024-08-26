f6651b351dd7765f7879eaea831d0436032ce1ce
DevRushi-engg <rgurav984@gmail.com>
2024-08-26 19:11:18 +0530

    minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
+print("Initializing Flask app...")
+
 from flask import Flask
 
 def create_app():
+    print("Creating Flask app...")
     app = Flask(__name__)
 
     @app.route('/')
     def index():
-        return 'Hello World!'
+        return 'Hello, World!'
 
     return app
